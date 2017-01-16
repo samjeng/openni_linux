fa7d376ddbf923b4166a7f8a5b38ba79297f8aa5
Alen Chen <alenchen@lips-hci.com>
2017-01-16 19:33:33 +0800

    Add Point cloud sample

--- NiUDPClient/NiUDPClient.cpp
+++ NiUDPClient/NiUDPClient.cpp
@@ -2,6 +2,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/viz/vizcore.hpp>
+#include <opencv2/viz/viz3d.hpp>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -25,6 +27,40 @@
 using namespace std;
 using namespace cv;
 
+Mat computeCloud( const Mat depthMap,
+                  const float fx,
+                  const float fy,
+                  const float cx,
+                  const float cy )
+{
+    Mat depth;
+    depthMap.convertTo( depth, CV_32F );
+
+    Size nsize = depthMap.size();
+    vector<Mat> output( 3 );
+    output[0] = Mat( nsize, CV_32F );
+    output[1] = Mat( nsize, CV_32F );
+    output[2] = depth;
+
+    for ( int i = 0; i < nsize.width; i++ )
+    {
+        output[0].col(i) = i;
+    }
+    for ( int j = 0; j < nsize.height; j++ )
+    {
+        output[1].row(j) = j;
+    }
+
+    float tmpx = 1.0 / fx;
+    float tmpy = 1.0 / fy;
+    output[0] = ( output[0] - cx).mul( output[2] ) * tmpx;
+    output[1] = ( output[1] - cy).mul( output[2] ) * tmpy;
+
+    Mat outMat;
+    merge( output, outMat );
+    return outMat;
+}
+
 int main( int argc, char* argv[] )
 {
     int socket_fd;
@@ -37,6 +73,8 @@ int main( int argc, char* argv[] )
     struct sockaddr_in myaddr;
     struct sockaddr_in server_addr;
     struct hostent *hp;
+
+    viz::Viz3d mPCWindow("VIZ Demo");
 
     if ( argc < 3 ) {
         cout << "No parameter" << endl;
@@ -73,6 +111,13 @@ int main( int argc, char* argv[] )
     char serial[11] = {0};
     char now_serial[11] = {0};
     int counter = 0;
+    float fx, fy, cx, cy;
+    fx = 525.0f;
+    fy = 525.0f;
+    cx = 319.5f;
+    cy = 239.5f;
+
+    mPCWindow.showWidget( "Coordinate Widget", viz::WCoordinateSystem( 400.0 ) );
     while ( true )
     {
         recvfrom(socket_fd, data1, PKT_SIZE + FID_SIZE, 0, (struct sockaddr*)&server_addr, (socklen_t *)&size);
@@ -91,9 +136,15 @@ int main( int argc, char* argv[] )
 #endif
                 Mat imgDepth( IMG_HEIGHT, IMG_WIDTH, CV_16UC1, ( void* )data_all );
                 Mat img8bitDepth;
-                imgDepth.convertTo( img8bitDepth, CV_8U, 255.0 / 5000 );
+                imgDepth.convertTo( img8bitDepth, CV_8U, 255.0 / 4096.0 );
                 imshow( "Depth view", img8bitDepth );
                 waitKey( 1 );
+                Mat mPointCloud = computeCloud( imgDepth, fx, fy, cx, cy);
+                applyColorMap( img8bitDepth, img8bitDepth, COLORMAP_JET);
+                viz::WCloud pointCloud = viz::WCloud( mPointCloud, img8bitDepth);
+                mPCWindow.showWidget( "Depth", pointCloud);
+                mPCWindow.spinOnce();
+                mPCWindow.removeWidget( "Depth" );
             }
             counter = 0;
         }
