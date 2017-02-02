24b1015f9d483c0524c89106cb0588e874109b7c
jimmyhong <jimmyhong@lips-hci.com>
2017-02-02 16:15:12 +0800

    Add fps value on Depth and RGB view.

--- NiSimpleViewer/NiSimpleViewer.cpp
+++ NiSimpleViewer/NiSimpleViewer.cpp
@@ -21,8 +21,13 @@ int main( int argc, char* argv[] )
     mImageGen.Create( mContext );
 
     mContext.StartGeneratingAll();
+
+    char fpsstr[7];
+    double tStart = 0;
+
     while ( true )
     {
+        tStart = (double)getTickCount();
         mContext.WaitAndUpdateAll();
 
         DepthMetaData depthData;
@@ -30,6 +35,8 @@ int main( int argc, char* argv[] )
         Mat imgDepth( depthData.FullYRes(), depthData.FullXRes(), CV_16UC1, ( void* )depthData.Data() );
         Mat img8bitDepth;
         imgDepth.convertTo( img8bitDepth, CV_8U, 255.0 / 5000 );
+        sprintf(fpsstr, "%.2f", 1.0 / (((double)getTickCount() - tStart) / getTickFrequency()));
+        putText(img8bitDepth, string("FPS:") + fpsstr, Point(5, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(200, 0, 0));
         imshow( "Depth view", img8bitDepth );
 
         ImageMetaData colorData;
@@ -37,6 +44,8 @@ int main( int argc, char* argv[] )
         Mat imgColor( colorData.FullYRes(), colorData.FullXRes(), CV_8UC3, ( void* )colorData.Data() );
         Mat imgBGRColor;
         cvtColor( imgColor, imgBGRColor, CV_RGB2BGR );
+        sprintf(fpsstr, "%.2f", 1.0 / (((double)getTickCount() - tStart) / getTickFrequency()));
+        putText(imgBGRColor, string("FPS:") + fpsstr, Point(5, 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(200, 0, 0));
         imshow( "Color view", imgBGRColor );
 
         waitKey( 1 );
